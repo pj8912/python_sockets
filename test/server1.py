bd0dcdc4fbe12413dc5c8600a62a160f07cafa9a
pj8912 <jpintofun2b@gmail.com>
2022-08-14 14:59:11 +0530

    added server class to test

---
+++ test/server1.py
@@ -0,0 +1,69 @@
+import socket
+import threading
+import sys
+
+class Server:
+    
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.host, self.port))
+        self.sock.listen()
+        self.clients = []
+
+        
+
+    def handle(self, client, address):
+        while True:
+            try:
+                message = client.recv(1024).decode('utf-8')
+                
+                if message == "dead":
+                    print(f"Client {str(address)} is disconnected")
+                    self.clients(client)
+                    self.clients.close()
+
+                elif message == "alive":
+                    print(f"Client {str(address)} is {message}")
+                    if client not in self.clients:
+                        self.clients.append(client)
+
+
+            except:
+                sys.exit(0)
+
+
+    def receive(self):
+        while True:
+            client, address = self.sock.accept()
+            print('------------------------------')
+            print('New Connection: '+ str(address))
+            print()
+            self.clients.append(client)
+
+            client.send("You are connected to the server".encode('utf-8'))
+            try:
+                thread = threading.Thread(target=self.handle, args=(client,address))
+                thread.daemon = True
+                thread.start()
+            except:
+                print("Cannot make thread connection..")
+
+
+def main():
+    server = Server('localhost', 9000)
+    while True:
+     try:
+         print('server started...')
+         server.receive()
+         
+     except KeyboardInterrupt:
+         print("closing server..")
+         sys.exit(0)
+     except:
+         print("connection error")
+
+if __name__ == '__main__':
+    main()
